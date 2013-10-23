0c386386e45d629068a7cc8ba6dd5b817808d27f
Dan Poggi <dan.poggi@gmail.com>
2013-10-23 01:38:52 -0400

    Taught Game how to sort itself, abstracted away some of the responsibilities of the content script

--- data/widget.js
+++ data/widget.js
@@ -1,43 +1,12 @@
-/*******************************************************************************
- * Monkey-patching String BECAUSE I CAN AHAHAHAHAHAHAHAHAHA
- ******************************************************************************/
-String.prototype.splice = function (idx, rem, s) {
-  return (this.slice(0, idx) + s + this.slice(idx + Math.abs(rem)));
-};
-
 (function () {
-  var games = [];
-  var LOLY2K = function (dateStr) {
-    return dateStr.splice(dateStr.length - 2, 0, '20');
-  };
-  var gameSorter = function (a, b) {
-    var aDate = new Date(), bDate = new Date();
-    aDate.setTime(Date.parse(LOLY2K(a.date)));
-    bDate.setTime(Date.parse(LOLY2K(b.date)));
-
-    if (aDate.getTime() < bDate.getTime()) {
-      return -1;
-    }
-    if (aDate.getTime() > bDate.getTime()) {
-      return 1;
-    }
-    return 0;
-  };
-
   self.port.on("schedule", function (schedule) {
-    for (var sport in schedule)
-      games = games.concat(schedule[sport].games);
-  });
+    var tbody = document.getElementById('schedule-body');
 
-  self.port.on("schedulesSent", function (num) {
-    games.sort(gameSorter);
-
-    games.forEach(function (game) {
-      var tbody = document.getElementById('schedule-body');
-
+    schedule.forEach(function (game) {
       var el = document.createElement('tr');
+
       el.innerHTML = "<td>" + game.sport + "</td>" +
-                     "<td>" + game.date + "</td>" +
+                     "<td>" + game.dateString + "</td>" +
                      "<td>" + game.event + "</td>" +
                      "<td>" + game.location + "</td>" +
                      "<td>" + game.result + "</td>";
