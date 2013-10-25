84949031a62189ca8d0861d6a87ad2aa4f610a14
Dan Poggi <dan.poggi@gmail.com>
2013-10-25 16:50:21 -0400

    Game now interprets times for games that haven't occurred yet

--- data/widget.js
+++ data/widget.js
@@ -3,13 +3,19 @@
     var tbody = document.getElementById('schedule-body');
 
     schedule.forEach(function (game) {
+      var result;
       var el = document.createElement('tr');
+
+      if (game.history)
+        result = (game.win ? 'Win' : 'Loss') + ', ' + game.score;
+      else
+        result = game.timeString;
 
       el.innerHTML = "<td>" + game.sport + "</td>" +
                      "<td>" + game.dateString + "</td>" +
-                     "<td>" + game.event + "</td>" +
+                     "<td>" + game.opponent + "</td>" +
                      "<td>" + game.location + "</td>" +
-                     "<td>" + game.result + "</td>";
+                     "<td>" + result + "</td>";
       tbody.appendChild(el);
     });
   });
